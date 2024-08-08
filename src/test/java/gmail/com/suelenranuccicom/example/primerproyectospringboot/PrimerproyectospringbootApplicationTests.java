ba6fe6aaecd0858deb0766ede1f78ed09c0b860b
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-08 12:08:16 +0200

    Initial commit

---
+++ src/test/java/gmail/com/suelenranuccicom/example/primerproyectospringboot/PrimerproyectospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package gmail.com.suelenranuccicom.example.primerproyectospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerproyectospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
