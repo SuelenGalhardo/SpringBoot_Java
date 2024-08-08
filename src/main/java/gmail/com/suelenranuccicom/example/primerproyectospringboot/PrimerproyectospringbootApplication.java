814142d93d09cb330a790512226cbfbd5f171411
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-08 12:08:16 +0200

    Initial commit

---
+++ src/main/java/gmail/com/suelenranuccicom/example/primerproyectospringboot/PrimerproyectospringbootApplication.java
@@ -0,0 +1,13 @@
+package gmail.com.suelenranuccicom.example.primerproyectospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerproyectospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerproyectospringbootApplication.class, args);
+	}
+
+}
