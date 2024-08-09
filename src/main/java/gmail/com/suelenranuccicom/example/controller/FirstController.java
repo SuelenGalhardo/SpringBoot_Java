e17768ba5bb14e550bb134db544cc18992d40eb6
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-09 21:30:25 +0200

    added first controller

---
+++ src/main/java/gmail/com/suelenranuccicom/example/controller/FirstController.java
@@ -0,0 +1,33 @@
+package gmail.com.suelenranuccicom.example.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Controller")
+
+public class FirstController {
+    @GetMapping("/firstmethod/{id}")
+    public String firstMethod(@PathVariable String id) {
+        return "el paramétro es " + id;
+    }
+
+    @GetMapping("/methodWithQueryParams")
+    public String methodWithQueryParams(@RequestParam String id) {
+
+        return "el paramétro es metodo QueryParamns es " + id;
+
+    }
+    @GetMapping("/methodWithQueryParams2")
+    public String methodWithQueryParams2(@RequestParam Map<String, String> allParams) {
+
+        return "el paramétro es metodo QueryParamns2 es " + allParams.entrySet();
+
+    }
+
+}
--- src/main/java/gmail/com/suelenranuccicom/example/primerproyectospringboot/PrimerproyectospringbootApplication.java
+++ src/main/java/gmail/com/suelenranuccicom/example/primerproyectospringboot/PrimerproyectospringbootApplication.java
@@ -2,8 +2,12 @@ package gmail.com.suelenranuccicom.example.primerproyectospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "gmail.com.suelenranuccicom")
+
 public class PrimerproyectospringbootApplication {
 
 	public static void main(String[] args) {
