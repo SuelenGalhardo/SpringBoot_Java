028669cc570aaadc7acab6eae48b2706dca79096
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-10 17:30:08 +0200

    added HeaderParamns and responseEntity

--- src/main/java/gmail/com/suelenranuccicom/example/controller/FirstController.java
+++ src/main/java/gmail/com/suelenranuccicom/example/controller/FirstController.java
@@ -2,10 +2,13 @@ package gmail.com.suelenranuccicom.example.controller;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +39,33 @@ public class FirstController {
     @PostMapping("/methodWithBodyParams")
     public String methodWithBodyParams(@RequestBody User user) {
         return "methodWithBodyParams" + user.username();
+    }
+
+    @PostMapping("/methodWithHeaders")
+
+    public String methodWithHeaders(@RequestHeader("name") String name) {
+        return "methodWithHeaders" + name;
+    }
+
+    @PostMapping("/methodWithListHeaders")
+    public String methodWithListHeaders(@RequestHeader Map<String, String> headers) {
+        return "methodWithListHeaders" + headers.entrySet();
+    }
+
+    @GetMapping("/methodResponseEntity/{id}")
+    public ResponseEntity<Object> methodResponseEntity(@PathVariable long id) {
+
+    
+        var user = new User ( "Suelen");
+
+        if (id > 5 ) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Mensagen de error");
+
+        }
+
+        return ResponseEntity.badRequest().body("numero menor que 5");
+
+   
     }
 
     record User(String username) {
