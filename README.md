36bfb2e11e60305a1ecd567b1ede6699ba69c112
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-10 16:16:53 +0200

    added Body Params @requestBody and record

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Spring Boot Framework Fundamentals:
 
 Building an Application with Spring Boot
 https://spring.io/guides/gs/spring-boot
+
+Requisiciones , consultas, test api: 
+https://apidog.com/download
--- src/main/java/gmail/com/suelenranuccicom/example/controller/FirstController.java
+++ src/main/java/gmail/com/suelenranuccicom/example/controller/FirstController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +25,20 @@ public class FirstController {
         return "el paramétro es metodo QueryParamns es " + id;
 
     }
+
     @GetMapping("/methodWithQueryParams2")
     public String methodWithQueryParams2(@RequestParam Map<String, String> allParams) {
 
         return "el paramétro es metodo QueryParamns2 es " + allParams.entrySet();
+
+    }
+
+    @PostMapping("/methodWithBodyParams")
+    public String methodWithBodyParams(@RequestBody User user) {
+        return "methodWithBodyParams" + user.username();
+    }
+
+    record User(String username) {
 
     }
 
